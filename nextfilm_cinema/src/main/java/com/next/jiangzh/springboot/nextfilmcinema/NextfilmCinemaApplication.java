a9a335bd3cd899fd8d5e09699266aa88779026b2
jiangzh <jiangzh@sina.com>
2019-07-14 12:47:05 +0800

    Cinema和Order服务与Eureka Client整合

--- nextfilm_cinema/src/main/java/com/next/jiangzh/springboot/nextfilmcinema/NextfilmCinemaApplication.java
+++ nextfilm_cinema/src/main/java/com/next/jiangzh/springboot/nextfilmcinema/NextfilmCinemaApplication.java
@@ -2,8 +2,10 @@ package com.next.jiangzh.springboot.nextfilmcinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NextfilmCinemaApplication {
 
     public static void main(String[] args) {
--- nextfilm_order/src/main/java/com/next/jiangzh/springboot/nextfilmorder/NextfilmOrderApplication.java
+++ nextfilm_order/src/main/java/com/next/jiangzh/springboot/nextfilmorder/NextfilmOrderApplication.java
@@ -2,8 +2,10 @@ package com.next.jiangzh.springboot.nextfilmorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NextfilmOrderApplication {
 
     public static void main(String[] args) {
