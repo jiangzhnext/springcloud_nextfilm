70f7ba063128b708bbe6e37ba91cb1274602fc92
jiangzh <jiangzh@sina.com>
2019-07-14 12:36:44 +0800

    Eureka Server环境构建

--- nextfilm_eureka_server/src/main/java/com/next/jiangzh/springboot/nextfilmeurekaserver/NextFilmEurekaServerApplication.java
+++ nextfilm_eureka_server/src/main/java/com/next/jiangzh/springboot/nextfilmeurekaserver/NextFilmEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.next.jiangzh.springboot.nextfilmeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NextFilmEurekaServerApplication {
 
     public static void main(String[] args) {
