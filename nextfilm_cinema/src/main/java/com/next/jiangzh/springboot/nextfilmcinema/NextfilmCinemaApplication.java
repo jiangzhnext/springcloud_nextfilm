791bf9fb6c4868a512b2cd66befa65362b442b3a
jiangzh <jiangzh@sina.com>
2019-07-28 11:22:59 +0800

    增加Ribbon重试机制演示

--- nextfilm_cinema/src/main/java/com/next/jiangzh/springboot/nextfilmcinema/NextfilmCinemaApplication.java
+++ nextfilm_cinema/src/main/java/com/next/jiangzh/springboot/nextfilmcinema/NextfilmCinemaApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRetry
 //@RibbonClient(name = "orderService",configuration = MyRibbonConf.class)
 @MapperScan(basePackages = {"com.next.jiangzh.springboot.nextfilmcinema.dao.mapper"})
 public class NextfilmCinemaApplication {
