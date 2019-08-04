db584d5bacacaa0b5523cab869ed47c3a5f1b6d3
jiangzh <jiangzh@sina.com>
2019-08-04 17:06:01 +0800

    Hystrix-DashBoard讲解演示

--- nextfilm_cinema/src/main/java/com/next/jiangzh/springboot/nextfilmcinema/NextfilmCinemaApplication.java
+++ nextfilm_cinema/src/main/java/com/next/jiangzh/springboot/nextfilmcinema/NextfilmCinemaApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableRetry
 //@RibbonClient(name = "orderService",configuration = MyRibbonConf.class)
 @MapperScan(basePackages = {"com.next.jiangzh.springboot.nextfilmcinema.dao.mapper"})
