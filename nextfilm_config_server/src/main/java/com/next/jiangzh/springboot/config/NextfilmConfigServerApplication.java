729131380e1af605246d2690336b6a24a3612a8f
jiangzh <jiangzh@sina.com>
2019-08-30 09:45:18 +0800

    SpringCloud Config基础环境构建

---
+++ nextfilm_config_server/src/main/java/com/next/jiangzh/springboot/config/NextfilmConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.next.jiangzh.springboot.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextfilmConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextfilmConfigServerApplication.class, args);
+    }
+
+}
